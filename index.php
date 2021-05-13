2dc8b89ba5066f02a5a3a021d932cd8e4dd0fa8a
giridharan20 <73735100+giridharan20@users.noreply.github.com>
2021-05-13 14:02:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<?php include"head.php";?>
+<body>
+
+
+<?php include"top_nav.php";?>
+    <!-- Header Carousel -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url('images/s1.jpg');"></div>
+                <div class="carousel-caption">
+                    
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('images/s2.jpg');"></div>
+                <div class="carousel-caption">
+                   
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('images/s3.jpg');"></div>
+                <div class="carousel-caption">
+                    
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
+
+    
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>We expect your loyal feedback to improve our standard.For more details and any subject related queries..</p>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-primary btn-block" href="contact.php"><i class="fa fa-phone"></i> Call to Action</a>
+                </div>
+            </div>
+        </div>
+		
+		<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+		<img src='' width="100%" height="100%" id='ModalImg'>
+    </div>
+  </div>
+</div>
+
+        <hr>
+
+        <!-- Footer -->
+		<?php include"footer.php"; ?>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+	
+	$(".img-portfolio").click(function(){
+		var a=$(this).attr("src");
+		$("#ModalImg").attr("src",a);
+       $('#myModal').modal();
+    })
+    </script>
+
+</body>
+
+</html>
